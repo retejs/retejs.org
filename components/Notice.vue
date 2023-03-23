ee99ed7d541db2968d14b38eca990a40c895ae91
Ni55aN <huccah@gmail.com>
2023-03-23 19:18:30 +0200

    add info about v2

---
+++ components/Notice.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+.notice
+  | {{ $t('currentVersionNotice.youAreBrowsing') }}
+  | {{ $t('currentVersionNotice.checkOut') }}
+  | !{' '}
+  a(href="https://rete.js.org" target="_blank") {{ $t('currentVersionNotice.docsV1') }}
+  | {{ $t('currentVersionNotice.orGetStarted' )}}
+</template>
+
+<style lang="sass" scoped>
+.notice
+  width: 100%
+  background: #697984
+  color: white
+  padding: 0.2em
+  text-align: center
+  font-weight: 100
+  font-size: 0.9rem
+  a
+    color: wheat
+</style>
