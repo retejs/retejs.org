181ef0a0f2e70367e8c23f0cc4e5f908d768dbde
Ni55aN <huccah@gmail.com>
2023-03-28 13:18:39 +0300

    add components for Questions section

---
+++ components/content/Questions.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+Collapse.collapse(accordion)
+  slot
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+};
+</script>
+
+<style lang="sass">
+.collapse p
+  margin: 0
+</style>
