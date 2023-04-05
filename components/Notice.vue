821708dc91575b069569ebf6551ea52261daac94
Ni55aN <huccah@gmail.com>
2023-04-05 17:43:54 +0300

    animate notice text if it doesn't fit on the screen

--- components/Notice.vue
+++ components/Notice.vue
@@ -1,24 +1,42 @@
 <template lang="pug">
 .notice
-  | {{ $t('currentVersionNotice.youAreBrowsing') }}
-  | {{ $t('currentVersionNotice.checkOut') }}
-  | !{' '}
-  a(href="https://rete.js.org" target="_blank") {{ $t('currentVersionNotice.docsV1') }}
-  | {{ $t('currentVersionNotice.orGetStarted' )}}
+  .text
+    | {{ $t('currentVersionNotice.youAreBrowsing') }}
+    | {{ $t('currentVersionNotice.checkOut') }}
+    | !{' '}
+    a(href="https://rete.js.org" target="_blank") {{ $t('currentVersionNotice.docsV1') }}
+    | {{ $t('currentVersionNotice.orGetStarted' )}}
 </template>
 
 <style lang="sass" scoped>
 @import '@/assets/styles/vars.sass'
+$text-width: 700px
 
 .notice
   width: 100%
   background: #697984
   color: white
-  padding: 0.2em
   text-align: center
   font-weight: 100
   font-size: 0.9rem
   height: $header-offset
-  a
-    color: wheat
+  overflow: hidden
+  .text
+    white-space: nowrap
+    padding: 0.2em 1em
+    animation: marquee 15s linear infinite
+    a
+      color: wheat
+
+@keyframes marquee
+  0%
+    transform: translateX(0px)
+  10%
+    transform: translateX(0px)
+  50%
+    transform: translateX(min(0px, calc(0px - $text-width + 100vw)))
+  60%
+    transform: translateX(min(0px, calc(0px - $text-width + 100vw)))
+  100%
+    transform: translateX(0px)
 </style>
