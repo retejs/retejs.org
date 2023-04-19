5a3cd3cdb00352c137db8bf29994f4b73fc7efba
Ni55aN <huccah@gmail.com>
2023-04-19 14:14:10 +0300

    add alert block on top of pro example

--- components/content/ProExample.vue
+++ components/content/ProExample.vue
@@ -1,4 +1,10 @@
 <template lang="pug">
+IViewAlert.alert(show-icon)
+  .content
+    .text {{ $t('examplesPage.proExample.alert') }}
+    Button.button(:to="patreon.link" target="_blank") {{ $t('examplesPage.proExample.getAccess') }}
+  template(#icon)
+    Icon.icon(type="md-finger-print")
 BaseExample
   iframe(
     :src="src"
@@ -7,11 +13,17 @@ BaseExample
 </template>
 
 <script>
+import sponsor from '../../consts/sponsor.json';
 import BaseExample from '../shared/BaseExample.vue';
 
 export default {
   props: {
     src: String,
+  },
+  data() {
+    return {
+      ...sponsor,
+    };
   },
   components: {
     BaseExample,
@@ -21,6 +33,16 @@ export default {
 
 <style lang="sass" scoped>
 @import '@/assets/styles/media.sass'
+
+.alert
+  padding-right: 8px
+  .content
+    display: flex
+    align-items: center
+    .text
+      flex: 1
+  .icon
+    font-size: 1.25em
 
 iframe
   width: 100%
