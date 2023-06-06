d9d27cacd2aa719e9d393496ebf0a87b1255870b
Ni55aN <huccah@gmail.com>
2023-06-06 16:23:52 +0300

    components for reference buttons in MD content

---
+++ components/content/RefExample.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+Button(
+  shape="circle"
+  size="small"
+  icon="ios-code"
+  :to="link"
+)
+  | {{title}}
+</template>
+
+<script>
+
+export default {
+  props: ['title', 'link'],
+};
+</script>
