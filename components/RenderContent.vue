7bed89429b1c932caf6f8d1da1f9342bbcdf7ab1
Ni55aN <stoliarov.v.s@gmail.com>
2023-09-08 16:16:50 +0300

    deps: update nuxt and provide a workaround for async setup

--- components/RenderContent.vue
+++ components/RenderContent.vue
@@ -4,59 +4,57 @@ ContentRenderer(:value="data")
     slot(name="not-found")
 </template>
 
-<script>
-import {
-  defineComponent, computed, ref,
-} from 'vue';
+
+<script setup>
+// workaround for https://github.com/nuxt/nuxt/issues/23074
+import { computed, ref } from 'vue';
 import { getPreview, mainPreview } from '../shared/assets';
 import { useShareThis } from '../shared/sharethis';
 import host from '../consts/host.json';
 
-export default defineComponent({
-  props: ['path', 'share', 'title', 'largePreview'],
-  async setup(props) {
-    const path = props.path.replace(/\/$/, '');
-    const dataRef = ref(null);
-    // workaround for https://github.com/vuejs/core/issues/1409
-    if (props.share) useShareThis(computed(() => dataRef.value && props.share(dataRef.value)));
-
-    // eslint-disable-next-line no-undef
-    const { data } = await useAsyncData(path, () => queryContent(path).findOne());
-
-    if (!data.value) return { data: null };
-
-    const { image, description } = data.value;
-    const title = computed(() => `${props.title ? props.title(data.value) || data.value.title : data.value.title} - Rete.js`);
-    const imageSrc = image ? getPreview(image.src) : mainPreview;
-    // eslint-disable-next-line no-nested-ternary
-    const twitterCard = props.largePreview
-      ? (imageSrc.endsWith('.gif') ? 'player' : 'summary_large_image')
-      : 'summary';
-
-    dataRef.value = data.value;
-    // should be reactive for ShareThis so useServerSeoMeta isn't an option
-    // eslint-disable-next-line no-undef
-    useSeoMeta({
-      title,
-      ogTitle: title,
-      description,
-      ogDescription: description,
-      ogImage: imageSrc,
-      twitterCard,
-      twitterTitle: title,
-      twitterDescription: description,
-      twitterSite: 'rete_js',
-      twitterCreator: 'rete_js',
-      twitterPlayerWidth: 500,
-      twitterPlayerHeight: Math.ceil(500 / (16 / 9)),
-      twitterPlayer: `${host.url}/preview/${path.split('/').filter(Boolean).reverse()[0]}`,
-      twitterImage: imageSrc,
-      twitterImageSrc: imageSrc,
-    });
-
-    return {
-      data,
-    };
-  },
+const props = defineProps({
+  path: String,
+  share: Function,
+  title: Function,
+  largePreview: Boolean
 });
+
+const path = props.path.replace(/\/$/, '');
+const dataRef = ref(null);
+// workaround for https://github.com/vuejs/core/issues/1409
+if (props.share) useShareThis(computed(() => dataRef.value && props.share(dataRef.value)));
+
+// eslint-disable-next-line no-undef
+const { data } = await useAsyncData(path, () => queryContent(path).findOne());
+
+if (data.value) {
+  const { image, description } = data.value;
+  const title = computed(() => `${props.title ? props.title(data.value) || data.value.title : data.value.title} - Rete.js`);
+  const imageSrc = image ? getPreview(image.src) : mainPreview;
+  // eslint-disable-next-line no-nested-ternary
+  const twitterCard = props.largePreview
+    ? (imageSrc.endsWith('.gif') ? 'player' : 'summary_large_image')
+    : 'summary';
+
+  dataRef.value = data.value;
+  // should be reactive for ShareThis so useServerSeoMeta isn't an option
+  // eslint-disable-next-line no-undef
+  useSeoMeta({
+    title,
+    ogTitle: title,
+    description,
+    ogDescription: description,
+    ogImage: imageSrc,
+    twitterCard,
+    twitterTitle: title,
+    twitterDescription: description,
+    twitterSite: 'rete_js',
+    twitterCreator: 'rete_js',
+    twitterPlayerWidth: 500,
+    twitterPlayerHeight: Math.ceil(500 / (16 / 9)),
+    twitterPlayer: `${host.url}/preview/${path.split('/').filter(Boolean).reverse()[0]}`,
+    twitterImage: imageSrc,
+    twitterImageSrc: imageSrc,
+  });
+}
 </script>
