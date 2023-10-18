c14bdb9c384398413b56f7a1e33b415d2aacb2f2
Ni55aN <stoliarov.v.s@gmail.com>
2023-10-18 19:06:57 +0300

    fix: diagrams generation

--- scripts/update-diagrams.bash
+++ scripts/update-diagrams.bash
@@ -1,8 +1,10 @@
+set -e
+
 source_dir="./assets/diagrams/"
 dest_dir="./public/diagrams/"
 
 docker run -it -v $(pwd):/data rlespinasse/drawio-export -f svg --remove-page-suffix -o _export
 
-rm -rf $dest_dir/{**/,}*.svg
+rm -rf $dest_dir
 rsync -av --include='*/' --include='_export/*' --exclude='*' "$source_dir/" "$dest_dir/"
 find $dest_dir -type f -name '*.svg' -exec sh -c 'newpath=$(echo {} | sed "s/_export\///"); mv {} $newpath' \;
