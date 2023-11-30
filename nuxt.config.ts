a060d478e054a1dc82204dfbd04165552efb9324
Maxim Slipenko <maxim@slipenko.com>
2023-11-30 18:43:32 +0000

    add ru locale in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -38,6 +38,7 @@ export default defineNuxtConfig({
     locales: [
       { code: 'en', file: 'en.json' },
       { code: 'uk', file: 'uk.json' },
+      { code: 'ru', file: 'ru.json' },
     ],
     detectBrowserLanguage: {
       fallbackLocale: 'en',
