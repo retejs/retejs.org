fb8de16332f4f91abce0cc0e221a4e42980d816c
Ni55aN <stoliarov.v.s@gmail.com>
2024-02-10 20:36:33 +0200

    fix: optimize styles

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 /// <reference types="nuxt" />
 import { isCI, isDevelopment } from 'std-env';
+import { version as iViewVersion } from 'view-ui-plus/package.json';
 import TypeDoc from './typedoc/index.mjs';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
@@ -16,7 +17,6 @@ export default defineNuxtConfig({
   telemetry: false,
   css: [
     '~/assets/styles/global.css',
-    'view-ui-plus/dist/styles/viewuiplus.css',
   ],
   content: {
     highlight: {
@@ -52,11 +52,15 @@ export default defineNuxtConfig({
       title: 'Rete.js',
       link: [
         { rel: 'icon', href: '/favicon.ico', sizes: 'any' },
+        { rel: 'stylesheet', href: `https://cdn.jsdelivr.net/npm/view-ui-plus@${iViewVersion}/dist/styles/viewuiplus.css` },
       ],
       meta: [
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      ]
+      ],
     },
+  },
+  features: {
+    inlineStyles: false,
   },
   pwa: {
     mode: isCI ? 'production' : 'development',
