ecc0adedee36913c5977d3a1cf61daad46950fb7
Ni55aN <stoliarov.v.s@gmail.com>
2024-02-18 13:37:19 +0200

    fix: sentry init

--- plugins/sentry.client.js
+++ plugins/sentry.client.js
@@ -1,26 +1,27 @@
+import {
+  init, browserTracingIntegration, BrowserProfilingIntegration, replayIntegration,
+} from '@sentry/vue';
 import { dsn } from '../config/sentry';
 
 // eslint-disable-next-line no-undef
 export default defineNuxtPlugin(async (nuxtApp) => {
   const app = nuxtApp.vueApp;
 
-  const { BrowserTracing } = await import('@sentry/browser');
-  const Sentry = await import('@sentry/vue');
-
-  Sentry.init({
+  init({
     app,
     dsn,
     environment: window.location.hostname === 'localhost' ? 'localhost' : 'production',
     integrations: [
-      new BrowserTracing({
-        routingInstrumentation: Sentry.vueRouterInstrumentation(nuxtApp.$router),
+      browserTracingIntegration({
+        router: nuxtApp.$router,
         tracePropagationTargets: ['localhost', 'rete.js.org', /^\//],
       }),
-      new Sentry.BrowserProfilingIntegration(),
-      Sentry.replayIntegration(),
+      new BrowserProfilingIntegration(),
+      replayIntegration(),
     ],
-    replaysSessionSampleRate: 0.25,
+    replaysSessionSampleRate: 0.5,
     replaysOnErrorSampleRate: 1.0,
     tracesSampleRate: 1.0,
+    profilesSampleRate: 1.0,
   });
 });
