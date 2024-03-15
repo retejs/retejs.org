8025df05acef6b131c7f1457994e6b11654fec76
Ni55aN <stoliarov.v.s@gmail.com>
2024-03-15 22:46:15 +0200

    types: update

--- src/sw.ts
+++ src/sw.ts
@@ -1,9 +1,17 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-restricted-globals */
 import { cacheNames, clientsClaim } from 'workbox-core';
 import { registerRoute } from 'workbox-routing';
 import { NetworkFirst } from 'workbox-strategies';
 
-const credentials = 'same-origin';
-const manifest = self.__WB_MANIFEST as Array<ManifestEntry>;
+declare const self: Window & typeof globalThis & {
+  location: string
+  __WB_MANIFEST: { url: string, href: string }[]
+  skipWaiting(): void;
+};
+
+const credentials = 'same-origin' as const;
+const manifest = self.__WB_MANIFEST;
 
 const cacheName = cacheNames.runtime;
 const cacheEntries: RequestInfo[] = [];
@@ -12,14 +20,15 @@ const manifestURLs = manifest.map(
   (entry) => {
     const url = new URL(entry.url, self.location);
     cacheEntries.push(new Request(url.href, {
-      credentials: credentials as any,
+      credentials,
     }));
     return url.href;
   },
 );
 
-self.addEventListener('install', (event: ExtendableEvent) => {
-  event.waitUntil(
+self.addEventListener('install', (event) => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  (event as any).waitUntil(
     caches.open(cacheName).then((cache) => cache.addAll(cacheEntries)),
   );
 });
