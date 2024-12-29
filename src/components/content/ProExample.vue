663c7419803fd49ec35e912671c8f612b0568e6c
Ni55aN <stoliarov.v.s@gmail.com>
2024-12-29 14:57:27 +0200

    refactor: banner component

--- src/components/content/ProExample.vue
+++ src/components/content/ProExample.vue
@@ -1,15 +1,13 @@
 <template lang="pug">
-IViewAlert.alert(show-icon)
-  .content
-    .text {{ $t('examplesPage.proExample.alert') }}
-    Tooltip(placement="top")
-      template(#content)
-        .tooltip {{ $t('examplesPage.proExample.info') }}
-      Button.question(type="text" shape="circle")
-        Icon.icon(name="f7:question-circle")
-    Button.button(:to="patreon.link" target="_blank") {{ $t('examplesPage.proExample.getAccess') }}
-  template(#icon)
-    Icon.icon(name="ion:finger-print")
+Banner(
+  icon="ion:finger-print"
+  :tooltip="{ icon: 'f7:question-circle' }"
+  :action="{ label: $t('examplesPage.proExample.getAccess') }"
+  @action="openPatreon"
+)
+  template(v-slot:text) {{ $t('examplesPage.proExample.alert') }}
+  template(v-slot:tooltip) {{ $t('examplesPage.proExample.info') }}
+
 BaseExample
   iframe(
     :src="src"
@@ -19,6 +17,7 @@ BaseExample
 
 <script lang="ts">
 import sponsor from '../../consts/sponsor.json'
+import Banner from '../shared/Banner.vue'
 import BaseExample from '../shared/BaseExample.vue'
 
 export default {
@@ -30,34 +29,19 @@ export default {
       ...sponsor
     }
   },
+  methods: {
+    openPatreon() {
+      window.open(this.patreon.link, '_blank')
+    }
+  },
   components: {
+    Banner,
     BaseExample
   }
 }
 </script>
 
 <style lang="sass" scoped>
-@import '@/assets/styles/media.sass'
-
-.alert
-  padding-right: 8px
-  .content
-    display: flex
-    align-items: center
-    gap: 0.5em
-    .text
-      flex: 1
-    .tooltip
-      text-align: center
-      max-width: 14em
-      text-wrap: wrap
-  .icon
-    font-size: 1.25em
-  .question
-    padding: 0.2em 0.5em
-    .icon
-      vertical-align: text-top
-
 iframe
   width: 100%
   height: 100%
